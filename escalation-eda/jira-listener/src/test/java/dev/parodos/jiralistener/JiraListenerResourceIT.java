e2edd59476f7903e63ee67063b2e40dac0fc17c8
Moti Asayag <masayag@redhat.com>
2024-03-19 11:00:52 +0200

    Remove obsolete examples
    Most of the examples are obsolete or were matured to be added to
    parodos-dev/serverless-workflows repository.
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- escalation-eda/jira-listener/src/test/java/dev/parodos/jiralistener/JiraListenerResourceIT.java
+++
@@ -1,8 +0,0 @@
-package dev.parodos.jiralistener;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class JiraListenerResourceIT extends JiraListenerResourceTest {
-    // Execute the same tests but in packaged mode.
-}
